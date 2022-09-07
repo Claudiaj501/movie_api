d28a5b11afece1498765850cf73f838c006847fa
Claudiaj501 <claudiaj.501@outlook.com>
2022-09-07 08:57:48 +0200

    mongoose changes

--- models.js
+++ models.js
@@ -24,6 +24,7 @@ let userSchema = mongoose.Schema({
   Password: {type: String, required: true},
   Email: {type: String, required: true},
   Birthday: Date,
+  // FavouriteMovies: [{ type: Schema.Types.ObjectId, ref: 'Movie' }]
   FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
