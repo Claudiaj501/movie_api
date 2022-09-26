098710516caaeed68115cadb3a95c8858c523757
Claudiaj501 <claudiaj.501@outlook.com>
2022-09-26 17:08:52 +0200

    change connection URI to mongoDB

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
 //Database connection
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(morgan('common'));
